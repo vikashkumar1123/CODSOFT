ac6a6db41a4519382925ccbc45b5245f1c3e804e
Vikash Kumar <150939696+vikashkumar1123@users.noreply.github.com>
2023-12-19 19:37:43 +0530

    Add files via upload

---
+++ TASK_03/script.js
@@ -0,0 +1,26 @@
+
+let displayValue = "";
+
+function appendToDisplay(value) {
+  displayValue += value;
+  updateDisplay();
+}
+
+function clearDisplay() {
+  displayValue = "";
+  updateDisplay();
+}
+
+function calculateResult() {
+  try {
+    displayValue = eval(displayValue).toString();
+    updateDisplay();
+  } catch (error) {
+    displayValue = "Error";
+    updateDisplay();
+  }
+}
+
+function updateDisplay() {
+  document.getElementById("display").innerText = displayValue;
+}
